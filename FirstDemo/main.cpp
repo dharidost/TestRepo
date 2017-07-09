231c6bdf333aa9bb80c0832a7bdb2c402d844de6
Dharmendra <dharidost@gmail.com>
2017-07-09 13:00:41 +0530

    Code Commit to Repo

---
+++ FirstDemo/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"main.h"
+using namespace std;
+
+int main()
+{
+	cout<<"Testing First Demo"<<endl;
+	return 0;
+}
---
+++ FirstDemo/main.h
@@ -0,0 +1,5 @@
+#ifndef TEST_DEMO
+#define TEST_DEMO
+
+
+#endif
